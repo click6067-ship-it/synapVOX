7f6a25bc1f040fbbc880e5613482a9d9c320da2d
click6067-ship-it <click6067@gmail.com>
2026-07-14 15:20:28 +0900

    feat(web): Vite React 스캐폴드 + 다크 디자인 토큰

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/tokens.css'
+
+function App() {
+  return (
+    <main>
+      <h1>SynapVox</h1>
+      <p style={{ color: 'var(--sub)' }}>web scaffold</p>
+    </main>
+  )
+}
+
+export default App
